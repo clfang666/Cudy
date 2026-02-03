44696358b1dbc3ba76ae19861c1a455c166a9255
clfang666 <1801420057@qq.com>
2026-02-03 19:05:09 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,7 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+# 修复 Rust 编译失败：强制关闭 download-ci-llvm
+# 解释：找到 Makefile 中启用 CI LLVM 下载的参数，将其设置为 false
+sed -i 's/$(TARGET_CONFIGURE_ARGS)/--set llvm.download-ci-llvm=false \\\n\t$(TARGET_CONFIGURE_ARGS)/' feeds/packages/lang/rust/Makefile
